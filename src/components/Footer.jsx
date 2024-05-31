81eeac38e56ffb68822686ad2197909b94f85e97
Aries <aries@Mac.local>
2024-05-31 14:47:10 +0800

    update layout and add new sections

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import { socials } from "../constants";
+import Section from "./Section";
+
+const Footer = () => {
+	return (
+		<Section crosses className="!px-0 !py-10 ">
+			<div className="container flex sm:justify-between items-center justify-center gap-10 max-sm:flex-col">
+				<p>{new Date().getFullYear()} © All rights reserved. </p>
+				<ul className="flex gap-5 flex-wrap">
+					{socials.map((item) => (
+						<a
+							key={item.id}
+							href={item.url}
+							target="_blank"
+							className="flex items-center justify-center rounded-full w-10 h-10 bg-n-7 transition-color hover:bg-n-5"
+						>
+							<img src={item.iconUrl} width={16} height={16} alt={item.title} />
+						</a>
+					))}
+				</ul>
+			</div>
+		</Section>
+	);
+};
+
+export default Footer;
